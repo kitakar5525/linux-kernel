aa9deac19cb466229cc24ebad2848c391a3506b6
Changlong Xie <changlongx.xie@intel.com>
2015-05-25 14:27:13 +0800

    Touchscreen: restore stay_awake after firmware update
    Change-Id: I2df65f52b8bbe06cc703d4628d9e3775a0fed845
    Tracked-On: https://jira01.devtools.intel.com/browse/MARVIN-652
    Signed-off-by: Changlong Xie <changlongx.xie@intel.com>
    Reviewed-on: https://android.intel.com:443/371505

--- drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_fw_update.c
+++ drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_fw_update.c
@@ -3073,6 +3073,7 @@ exit:
 static int fwu_start_reflash(void)
 {
 	int retval = 0;
+	bool stay_awake = false;
 	enum flash_area flash_area;
 	const struct firmware *fw_entry = NULL;
 	struct synaptics_rmi4_data *rmi4_data = fwu->rmi4_data;
@@ -3084,6 +3085,7 @@ static int fwu_start_reflash(void)
 		return -ENODEV;
 	}
 
+	stay_awake = rmi4_data->stay_awake;
 	rmi4_data->stay_awake = true;
 
 	pr_notice("%s: Start of reflash process\n", __func__);
@@ -3207,7 +3209,7 @@ exit:
 
 	pr_notice("%s: End of reflash process\n", __func__);
 
-	rmi4_data->stay_awake = false;
+	rmi4_data->stay_awake = stay_awake;
 
 	return retval;
 }

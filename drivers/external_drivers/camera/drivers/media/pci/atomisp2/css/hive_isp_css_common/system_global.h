a2600a3263a651994de5659023275f6009e3966c
Pengpeng Cui <pengpengx.cui@intel.com>
2015-08-26 18:12:58 +0800

    atomisp2: css2401 and 2401_legacy-irci_master_20150824_1500.
    Merge CSS2.1 for css2401 and 2401_legacy
    for irci_master_20150824_1500 firmware tag
    
    Change-Id: Ia72456b87b5e12d7092b197fa4d25b3019dc3111
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-43419
    Signed-off-by: Pengpeng Cui <pengpengx.cui@intel.com>
    Reviewed-on: https://icggerrit.ir.intel.com/71131
    Approver: WeiX Meng <weix.meng@intel.com>
    Integrator: WeiX Meng <weix.meng@intel.com>
    Build: WeiX Meng <weix.meng@intel.com>
    Reviewed-by: WeiX Meng <weix.meng@intel.com>
    Maintainer: WeiX Meng <weix.meng@intel.com>
    
    (cherry picked from commit 4ee6fd61c78e8fc306990c1e90774e6adcb8b409)
    [ Conflicts:
    	both modified:   drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/ia_css.h
    	both modified:   drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/ia_css_host_data.h
    	both modified:   drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/ia_css_types.h
    	both modified:   drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/isp/kernels/hdr/ia_css_hdr.host.c
    	both modified:   drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/isp/kernels/hdr/ia_css_hdr.host.h
    	both modified:   drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/isp/kernels/hdr/ia_css_hdr_param.h
    	both modified:   drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/isp/kernels/hdr/ia_css_hdr_types.h
     due to "/*" -> "/**" change under incoming change.
     accepted incoming change]
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/hive_isp_css_common/system_global.h
+++ drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/hive_isp_css_common/system_global.h
@@ -321,4 +321,26 @@ enum ia_css_isp_memories {
 #define N_IA_CSS_ISP_MEMORIES   IA_CSS_NUM_MEMORIES
 #define IA_CSS_NUM_ISP_MEMORIES IA_CSS_NUM_MEMORIES
 
+typedef enum {
+	dev_chn, /* device channels, external resource */
+	ext_mem, /* external memories */
+	int_mem, /* internal memories */
+	int_chn  /* internal channels, user defined */
+} resource_type_t;
+
+/* if this enum is extended with other memory resources, pls also extend the function resource_to_memptr() */
+typedef enum {
+	vied_nci_dev_chn_dma_ext0,
+	int_mem_vmem0,
+	int_mem_dmem0
+} resource_id_t;
+
+/* enum listing the different memories within a program group.
+   This enum is used in the mem_ptr_t type */
+typedef enum {
+	buf_mem_invalid = 0,
+	buf_mem_vmem_prog0,
+	buf_mem_dmem_prog0
+} buf_mem_t;
+
 #endif /* __SYSTEM_GLOBAL_H_INCLUDED__ */

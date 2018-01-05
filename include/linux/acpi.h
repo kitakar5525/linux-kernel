4122c097473e1abf4207695d1e9775051f2a571e
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2018-01-05 18:09:33 +0200

    (backport-v4.16) ACPI: utils: Introduce acpi_dev_get_first_match_name()
    Sometimes the user wants to have device name of the match rather than
    just checking if device present or not. To make life easier for such
    users introduce acpi_dev_get_first_match_name() helper based on code
    for acpi_dev_present().
    
    For example, GPIO driver for Intel Merrifield needs to know the device
    name of pin control to be able to apply GPIO mapping table to the proper
    device.
    
    To be more consistent with the purpose rename
    
      struct acpi_dev_present_info  -> struct acpi_dev_match_info
      acpi_dev_present_cb()         -> acpi_dev_match_cb()
    
    in the utils.c file.
    
    Tested-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    (cherry picked from commit 67dcf8a3e06582cb6b02952335b5612beb97889f)
    [ Conflicts:
    	include/linux/acpi.h]
    [backport for 817b4d64da03 ("ACPI / utils: Introduce acpi_dev_get_first_match_dev() helper")
     which is for atomisp_gmin_platform]
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -650,6 +650,12 @@ static inline bool acpi_dev_present(const char *hid, const char *uid, s64 hrv)
 	return false;
 }
 
+static inline struct acpi_device *
+acpi_dev_get_first_match_dev(const char *hid, const char *uid, s64 hrv)
+{
+	return NULL;
+}
+
 static inline void acpi_dev_put(struct acpi_device *adev) {}
 
 static inline bool is_acpi_node(struct fwnode_handle *fwnode)

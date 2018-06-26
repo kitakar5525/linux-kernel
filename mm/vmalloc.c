b46f17c15e06d526a38f9deb571407691a17f78f
Ben Hutchings <ben@decadent.org.uk>
2018-06-26 16:59:01 +0100

    Export symbols needed by Android drivers
    We want to enable use of the Android ashmem and binder drivers to
    support Anbox, but they should not be built-in as that would waste
    resources and increase security attack surface on systems that don't
    need them.
    
    Export the currently un-exported symbols they depend on.
    
    (Patch from here:
    - https://github.com/linux-surface/linux-surface/blob/3458d3e1cbae9aea5378cd7f83b0c4bcede6acfe/pkg/debian/kernel/export-symbols-needed-by-android-drivers.patch)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1263,6 +1263,7 @@ overflow:
 	kmem_cache_free(vmap_area_cachep, va);
 	return ERR_PTR(-EBUSY);
 }
+EXPORT_SYMBOL_GPL(map_kernel_range_noflush);
 
 int register_vmap_purge_notifier(struct notifier_block *nb)
 {
@@ -2117,6 +2118,7 @@ struct vm_struct *get_vm_area(unsigned long size, unsigned long flags)
 				  NUMA_NO_NODE, GFP_KERNEL,
 				  __builtin_return_address(0));
 }
+EXPORT_SYMBOL_GPL(get_vm_area);
 
 struct vm_struct *get_vm_area_caller(unsigned long size, unsigned long flags,
 				const void *caller)

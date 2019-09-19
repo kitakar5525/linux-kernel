1866166605fe6be5b2504e48e2957ebf0c90542f
Tsuchiya Yuto (kitakar5525) <kitakar@gmail.com>
2019-09-19 23:35:54 +0900

    ipu3: add ipu3 patches for chromeos-4.19
    (patches from bugzilla https://bugzilla.kernel.org/show_bug.cgi?id=201579
    ("201579 – HP Elite x2 1013 G3 unable to enter S0ix"))
    
    [Modified to match firmware location on Arch Linux
        -#define IMGU_FW_NAME			"ipu3-fw.bin"
        +#define IMGU_FW_NAME			"intel/ipu3-fw.bin"
    Concatenated below patchset:
        0001-v4l-Add-Intel-IPU3-meta-buffer-formats.patch
        0002-doc-rst-Add-Intel-IPU3-documentation.patch
        0003-v4l-Add-Intel-IPU3-meta-data-uAPI.patch
        0004-intel-ipu3-abi-Add-register-definitions-and-enum.patch
        0005-intel-ipu3-abi-Add-structs.patch
        0006-intel-ipu3-mmu-Implement-driver.patch
        0007-intel-ipu3-Implement-DMA-mapping-functions.patch
        0008-intel-ipu3-css-Add-dma-buff-pool-utility-functions.patch
        0009-intel-ipu3-css-Add-support-for-firmware-management.patch
        0010-intel-ipu3-css-Add-static-settings-for-image-pipelin.patch
        0011-intel-ipu3-css-Compute-and-program-ccs.patch
        0012-intel-ipu3-css-Initialize-css-hardware.patch
        0013-intel-ipu3-Add-css-pipeline-programming.patch
        0014-intel-ipu3-Add-v4l2-driver-based-on-media-framework.patch
        0015-intel-ipu3-Add-imgu-top-level-pci-device-driver.patch
        0016-intel-ipu3-add-dual-pipe-support.patch
        0017-v4l-Add-support-for-V4L2_BUF_TYPE_META_OUTPUT.patch
        0018-TEST-FOR-DEBUG-PRINTS.patch
        0019-ipu3-cio2-Allow-probe-to-succeed-if-there-are-no-sen.patch
        0020-platform-x86-intel_pmc_core-Show-Latency-Tolerance-i.patch
        0021-platform-x86-intel_pmc_core-Fix-LTR-IGNORE-Max-offse.patch
        0022-platform-x86-intel_pmc_core-Decode-Snoop-Non-Snoop-L.patch
        0023-platform-x86-intel_pmc_core-convert-to-DEFINE_SHOW_A.patch
        0024-platform-x86-intel_telemetry-convert-to-DEFINE_SHOW_.patch
    Resolved conflicts for chromeos-4.19]
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/platform/x86/intel_telemetry_debugfs.c
+++ drivers/platform/x86/intel_telemetry_debugfs.c
@@ -477,17 +477,7 @@ static int telem_pss_states_show(struct seq_file *s, void *unused)
 	return 0;
 }
 
-static int telem_pss_state_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, telem_pss_states_show, inode->i_private);
-}
-
-static const struct file_operations telem_pss_ops = {
-	.open		= telem_pss_state_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= single_release,
-};
+DEFINE_SHOW_ATTRIBUTE(telem_pss_states);
 
 static int telem_ioss_states_show(struct seq_file *s, void *unused)
 {
@@ -516,17 +506,7 @@ static int telem_ioss_states_show(struct seq_file *s, void *unused)
 	return 0;
 }
 
-static int telem_ioss_state_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, telem_ioss_states_show, inode->i_private);
-}
-
-static const struct file_operations telem_ioss_ops = {
-	.open		= telem_ioss_state_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= single_release,
-};
+DEFINE_SHOW_ATTRIBUTE(telem_ioss_states);
 
 static int telem_soc_states_show(struct seq_file *s, void *unused)
 {
@@ -675,17 +655,7 @@ static int telem_soc_states_show(struct seq_file *s, void *unused)
 	return 0;
 }
 
-static int telem_soc_state_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, telem_soc_states_show, inode->i_private);
-}
-
-static const struct file_operations telem_socstate_ops = {
-	.open		= telem_soc_state_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= single_release,
-};
+DEFINE_SHOW_ATTRIBUTE(telem_soc_states);
 
 static int telem_s0ix_res_get(void *data, u64 *val)
 {
@@ -971,7 +941,7 @@ static int __init telemetry_debugfs_init(void)
 
 	f = debugfs_create_file("pss_info", S_IFREG | S_IRUGO,
 				debugfs_conf->telemetry_dbg_dir, NULL,
-				&telem_pss_ops);
+				&telem_pss_states_fops);
 	if (!f) {
 		pr_err("pss_sample_info debugfs register failed\n");
 		goto out;
@@ -979,7 +949,7 @@ static int __init telemetry_debugfs_init(void)
 
 	f = debugfs_create_file("ioss_info", S_IFREG | S_IRUGO,
 				debugfs_conf->telemetry_dbg_dir, NULL,
-				&telem_ioss_ops);
+				&telem_ioss_states_fops);
 	if (!f) {
 		pr_err("ioss_sample_info debugfs register failed\n");
 		goto out;
@@ -987,7 +957,7 @@ static int __init telemetry_debugfs_init(void)
 
 	f = debugfs_create_file("soc_states", S_IFREG | S_IRUGO,
 				debugfs_conf->telemetry_dbg_dir,
-				NULL, &telem_socstate_ops);
+				NULL, &telem_soc_states_fops);
 	if (!f) {
 		pr_err("ioss_sample_info debugfs register failed\n");
 		goto out;

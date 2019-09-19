fd68e222c40d03907c55a0621971ac8b616baf2d
Tsuchiya Yuto (kitakar5525) <kitakar@gmail.com>
2019-09-19 23:35:54 +0900

    ipu3: add ipu3 patches for chromeos-4.19
    (patches from bugzilla https://bugzilla.kernel.org/show_bug.cgi?id=201579
    ("201579 – HP Elite x2 1013 G3 unable to enter S0ix"))
    
    [Modified to match firmware location on Arch Linux
        -#define IMGU_FW_NAME			"ipu3-fw.bin"
        +#define IMGU_FW_NAME			"intel/ipu3-fw.bin"
    Concatenated below patchset:
        0001-v4l-Add-Intel-IPU3-meta-buffer-formats.patch
        0002-doc-rst-Add-Intel-IPU3-documentation.patch
        0003-v4l-Add-Intel-IPU3-meta-data-uAPI.patch
        0004-intel-ipu3-abi-Add-register-definitions-and-enum.patch
        0005-intel-ipu3-abi-Add-structs.patch
        0006-intel-ipu3-mmu-Implement-driver.patch
        0007-intel-ipu3-Implement-DMA-mapping-functions.patch
        0008-intel-ipu3-css-Add-dma-buff-pool-utility-functions.patch
        0009-intel-ipu3-css-Add-support-for-firmware-management.patch
        0010-intel-ipu3-css-Add-static-settings-for-image-pipelin.patch
        0011-intel-ipu3-css-Compute-and-program-ccs.patch
        0012-intel-ipu3-css-Initialize-css-hardware.patch
        0013-intel-ipu3-Add-css-pipeline-programming.patch
        0014-intel-ipu3-Add-v4l2-driver-based-on-media-framework.patch
        0015-intel-ipu3-Add-imgu-top-level-pci-device-driver.patch
        0016-intel-ipu3-add-dual-pipe-support.patch
        0017-v4l-Add-support-for-V4L2_BUF_TYPE_META_OUTPUT.patch
        0018-TEST-FOR-DEBUG-PRINTS.patch
        0019-ipu3-cio2-Allow-probe-to-succeed-if-there-are-no-sen.patch
        0020-platform-x86-intel_pmc_core-Show-Latency-Tolerance-i.patch
        0021-platform-x86-intel_pmc_core-Fix-LTR-IGNORE-Max-offse.patch
        0022-platform-x86-intel_pmc_core-Decode-Snoop-Non-Snoop-L.patch
        0023-platform-x86-intel_pmc_core-convert-to-DEFINE_SHOW_A.patch
        0024-platform-x86-intel_telemetry-convert-to-DEFINE_SHOW_.patch
    Resolved conflicts for chromeos-4.19]
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

---
+++ drivers/media/pci/intel/ipu3/ipu3-css-pool.h
@@ -0,0 +1,56 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2018 Intel Corporation */
+
+#ifndef __IPU3_UTIL_H
+#define __IPU3_UTIL_H
+
+struct device;
+struct imgu_device;
+
+#define IPU3_CSS_POOL_SIZE		4
+
+/**
+ * ipu3_css_map - store DMA mapping info for buffer
+ *
+ * @size:		size of the buffer in bytes.
+ * @vaddr:		kernel virtual address.
+ * @daddr:		iova dma address to access IPU3.
+ * @vma:		private, a pointer to &struct vm_struct,
+ *			used for ipu3_dmamap_free.
+ */
+struct ipu3_css_map {
+	size_t size;
+	void *vaddr;
+	dma_addr_t daddr;
+	struct vm_struct *vma;
+};
+
+/**
+ * ipu3_css_pool - circular buffer pool definition
+ *
+ * @entry:		array with IPU3_CSS_POOL_SIZE elements.
+ * @entry.param:	a &struct ipu3_css_map for storing the mem mapping.
+ * @entry.framenum:	the css frame number, used to determine if the entry
+ *			is old enough to be recycled.
+ * @last:		write pointer, initialized to IPU3_CSS_POOL_SIZE.
+ */
+struct ipu3_css_pool {
+	struct {
+		struct ipu3_css_map param;
+		long framenum;
+	} entry[IPU3_CSS_POOL_SIZE];
+	unsigned int last; /* Latest entry */
+};
+
+int ipu3_css_dma_buffer_resize(struct imgu_device *imgu,
+			       struct ipu3_css_map *map, size_t size);
+void ipu3_css_pool_cleanup(struct imgu_device *imgu,
+			   struct ipu3_css_pool *pool);
+int ipu3_css_pool_init(struct imgu_device *imgu, struct ipu3_css_pool *pool,
+		       size_t size);
+int ipu3_css_pool_get(struct ipu3_css_pool *pool, long framenum);
+void ipu3_css_pool_put(struct ipu3_css_pool *pool);
+const struct ipu3_css_map *ipu3_css_pool_last(struct ipu3_css_pool *pool,
+					      unsigned int last);
+
+#endif

b90bc589557daf2fc333920568f4ae6c084de2ef
Tsuchiya Yuto <kitakar@gmail.com>
2020-07-11 15:42:45 +0900

    surface3-spi: add parameter use_dma
    This commit adds parameter "use_dma".
    
    The parameter is set to true by default so that the driver always uses
    DMA by default. Somehow some setups (observed in chromeos kernel) use
    PIO by default without this change.
    
    You can toggle the usage of DMA.
    
    Examples:
        # disable DMA
        $ echo N | sudo tee /sys/module/surface3_spi/parameters/use_dma
    
        # enable DMA
        $ echo Y | sudo tee /sys/module/surface3_spi/parameters/use_dma
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/input/touchscreen/surface3_spi.c
+++ drivers/input/touchscreen/surface3_spi.c
@@ -29,6 +29,11 @@
 #define SURFACE3_REPORT_TOUCH	0xd2
 #define SURFACE3_REPORT_PEN	0x16
 
+bool use_dma = true;
+module_param(use_dma, bool, 0644);
+MODULE_PARM_DESC(use_dma,
+				"Toggle DMA usage. (default: true.)");
+
 struct surface3_ts_data {
 	struct spi_device *spi;
 	struct gpio_desc *gpiod_rst[2];
@@ -330,6 +335,13 @@ static int surface3_spi_create_pen_input(struct surface3_ts_data *data)
 	return 0;
 }
 
+static bool surface3_spi_can_dma(struct spi_controller *ctlr,
+				struct spi_device *spi,
+				struct spi_transfer *tfr)
+{
+	return use_dma;
+}
+
 static int surface3_spi_probe(struct spi_device *spi)
 {
 	struct surface3_ts_data *data;
@@ -371,6 +383,11 @@ static int surface3_spi_probe(struct spi_device *spi)
 					  "Surface3-irq", data);
 	if (error)
 		return error;
+
+	/*
+	 * Set up DMA
+	 */
+	spi->controller->can_dma = surface3_spi_can_dma;
 
 	return 0;
 }

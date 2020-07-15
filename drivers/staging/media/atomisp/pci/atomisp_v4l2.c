54c5b02c5150479749e33724a4110d902256b595
Tsuchiya Yuto <kitakar@gmail.com>
2020-07-15 18:38:01 +0900

    atomisp: ISP2401: fix up the open_v load race
    Signed-off-by: Alan Cox <alan@xxxxxxxxxxxxxxx>
    
    (patch from here:
    - https://github.com/croutor/atomisp2401/blob/1f43922051e1d9b5b503ebc3c2ccac5cfab04975/0007-atomisp_ISP2401_fix_up_the_open_v_load_race.patch)
    [Surface 3 (Cherry Trail) often fails to init css:
    
        [  110.077904] kernel: atomisp-isp2 0000:00:03.0: detected 2 camera sensors
        [  110.077922] kernel: atomisp-isp2 0000:00:03.0: Entity type for entity ATOM ISP CSI2-port0 was not initialized!
        [  110.077931] kernel: atomisp-isp2 0000:00:03.0: Entity type for entity ATOM ISP CSI2-port1 was not initialized!
        [  110.077936] kernel: atomisp-isp2 0000:00:03.0: Entity type for entity ATOM ISP CSI2-port2 was not initialized!
        [  110.077941] kernel: atomisp-isp2 0000:00:03.0: Entity type for entity file_input_subdev was not initialized!
        [  110.077945] kernel: atomisp-isp2 0000:00:03.0: Entity type for entity tpg_subdev was not initialized!
        [  110.077962] kernel: atomisp-isp2 0000:00:03.0: Entity type for entity ATOMISP_SUBDEV_0 was not initialized!
        [  110.087433] kernel: atomisp-isp2 0000:00:03.0: Entity type for entity ATOMISP_SUBDEV_1 was not initialized!
        [  110.089568] kernel: atomisp-isp2 0000:00:03.0: Firmware version may not be compatible with this driver
        [  110.089574] kernel: atomisp-isp2 0000:00:03.0: Expecting version 'irci_ecr - master_20150911_0724', but firmware is 'irci_stable_candrpv_0415_20150521_0458'.
        [  110.141474] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
        [  110.184343] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
        [  110.185494] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
        [  110.186844] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
        [  110.188250] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
        [  110.203564] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
        [  110.220968] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
        [  110.249973] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
        [  110.258324] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
        [  110.265453] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
        [  110.266331] kernel: atomisp-isp2 0000:00:03.0: css init failed --- bad firmware?
    
    It seems that applying this patch fixes the initialization.]
    [Resolved path change]
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    
    [TODO: still needed for v5.14 ?]
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 7a65e7f014dfbfb8b46611f423d5e8cc8b08af2b)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/atomisp_v4l2.c
+++ drivers/staging/media/atomisp/pci/atomisp_v4l2.c
@@ -1566,6 +1566,7 @@ static int atomisp_pci_probe(struct pci_dev *pdev, const struct pci_device_id *i
 	dev_dbg(&pdev->dev, "atomisp mmio base: %p\n", isp->base);
 
 	rt_mutex_init(&isp->mutex);
+	rt_mutex_init(&isp->loading);
 	mutex_init(&isp->streamoff_mutex);
 	spin_lock_init(&isp->lock);
 
@@ -1748,6 +1749,8 @@ static int atomisp_pci_probe(struct pci_dev *pdev, const struct pci_device_id *i
 		pci_write_config_dword(pdev, MRFLD_PCI_CSI_AFE_TRIM_CONTROL, csi_afe_trim);
 	}
 
+	rt_mutex_lock(&isp->loading);
+
 	err = atomisp_initialize_modules(isp);
 	if (err < 0) {
 		dev_err(&pdev->dev, "atomisp_initialize_modules (%d)\n", err);
@@ -1805,6 +1808,8 @@ static int atomisp_pci_probe(struct pci_dev *pdev, const struct pci_device_id *i
 	release_firmware(isp->firmware);
 	isp->firmware = NULL;
 	isp->css_env.isp_css_fw.data = NULL;
+	isp->ready = true;
+	rt_mutex_unlock(&isp->loading);
 
 	atomisp_drvfs_init(isp);
 
@@ -1824,6 +1829,7 @@ wdt_work_queue_fail:
 register_entities_fail:
 	atomisp_uninitialize_modules(isp);
 initialize_modules_fail:
+	rt_mutex_unlock(&isp->loading);
 	cpu_latency_qos_remove_request(&isp->pm_qos);
 	atomisp_msi_irq_uninit(isp);
 	pci_free_irq_vectors(pdev);

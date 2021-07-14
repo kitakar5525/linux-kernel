d17b362fbc84fd3a9e35d9cd96da4c44496864e0
Tsuchiya Yuto <kitakar@gmail.com>
2021-07-14 21:30:59 +0900

    media: ipu3-cio2: (cio2-bridge) add INT3479 (ov5670)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/media/pci/intel/ipu3/cio2-bridge.c
+++ drivers/media/pci/intel/ipu3/cio2-bridge.c
@@ -24,6 +24,8 @@ static const struct cio2_sensor_config cio2_supported_sensors[] = {
 	CIO2_SENSOR_CONFIG("INT33BE", 0),
 	/* Omnivision OV2680 */
 	CIO2_SENSOR_CONFIG("OVTI2680", 0),
+	/* Omnivision OV5670 */
+	CIO2_SENSOR_CONFIG("INT3479", 0),
 	/* Omnivision OV8865 */
 	CIO2_SENSOR_CONFIG("INT347A", 1, 360000000),
 };

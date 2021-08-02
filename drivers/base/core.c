0c449f7d25c8dd0b4449390efdedea78feccc2f5
Tsuchiya Yuto <kitakar@gmail.com>
2021-08-03 06:30:34 +0900

    (upst-atomisp_gmin_platform) (partial-backport) device.h: introduce bus_find_device_by_acpi_dev()
    Ported from v5.14-rc3.
    
    To be used for atomisp_gmin_platform.
    
    To match v4.4, the following changes are made:
    - removed const for bus_find_device()
    - added linux/acpi.h header
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -10,6 +10,7 @@
  *
  */
 
+#include <linux/acpi.h>
 #include <linux/cpufreq.h>
 #include <linux/device.h>
 #include <linux/err.h>
@@ -2384,3 +2385,9 @@ void set_secondary_fwnode(struct device *dev, struct fwnode_handle *fwnode)
 	else
 		dev->fwnode = fwnode;
 }
+
+int device_match_acpi_dev(struct device *dev, void *adev)
+{
+	return ACPI_COMPANION(dev) == adev;
+}
+EXPORT_SYMBOL(device_match_acpi_dev);

c27c1a262ff897008ffe33fa8ae5d853305ba309
Tsuchiya Yuto <kitakar@gmail.com>
2021-08-02 22:17:33 +0900

    (HACK-ish) pmic: whiskeycove: add prefix xiaomi_mipad2_ for seemingly Xiaomi Mi Pad 2 specific things
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/mfd/intel_soc_pmic_wc.c
+++ drivers/mfd/intel_soc_pmic_wc.c
@@ -652,40 +652,40 @@ static void wc_set_thermal_pdata(void)
 				sizeof(whiskey_cove_thermal_data), 0);
 }
 
-static struct regulator_consumer_supply v1p8sx_consumer[] = {
+static struct regulator_consumer_supply xiaomi_mipad2_v1p8sx_consumer[] = {
 	REGULATOR_SUPPLY("v1p8sx", "OVTI5693:00"),
 	REGULATOR_SUPPLY("v1p8sx", "SS5K0008:00"),
 };
 
-static struct regulator_consumer_supply v2p8sx_consumer[] = {
+static struct regulator_consumer_supply xiaomi_mipad2_v2p8sx_consumer[] = {
 	REGULATOR_SUPPLY("v2p8sx", "OVTI5693:00"),
 	REGULATOR_SUPPLY("v2p8sx", "SS5K0008:00"),
 };
 
-static struct regulator_consumer_supply v1p2sx_consumer[] = {
+static struct regulator_consumer_supply xiaomi_mipad2_v1p2sx_consumer[] = {
 	REGULATOR_SUPPLY("v1p2sx", "OVTI5693:00"),
 	REGULATOR_SUPPLY("v1p2sx", "SS5K0008:00"),
 };
 
-static struct regulator_consumer_supply vprog4d_consumer[] = {
+static struct regulator_consumer_supply xiaomi_mipad2_vprog4d_consumer[] = {
 	REGULATOR_SUPPLY("vprog4d", "OVTI5693:00"),
 	REGULATOR_SUPPLY("vprog4d", "SS5K0008:00"),
 };
 
 /* v1p8sx regulator */
-static struct regulator_init_data v1p8sx_data = {
+static struct regulator_init_data xiaomi_mipad2_v1p8sx_data = {
 	.constraints = {
 		.min_uV = 1620000,
 		.max_uV = 1980000,
 		.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
 		.valid_modes_mask	= REGULATOR_MODE_NORMAL,
 	},
-	.num_consumer_supplies	= ARRAY_SIZE(v1p8sx_consumer),
-	.consumer_supplies	= v1p8sx_consumer,
+	.num_consumer_supplies	= ARRAY_SIZE(xiaomi_mipad2_v1p8sx_consumer),
+	.consumer_supplies	= xiaomi_mipad2_v1p8sx_consumer,
 };
 
 /* v2p8sx regulator */
-static struct regulator_init_data v2p8sx_data = {
+static struct regulator_init_data xiaomi_mipad2_v2p8sx_data = {
 	.constraints = {
 		.min_uV			= 2565000,
 		.max_uV			= 3300000,
@@ -693,31 +693,31 @@ static struct regulator_init_data v2p8sx_data = {
 					REGULATOR_CHANGE_STATUS,
 		.valid_modes_mask	= REGULATOR_MODE_NORMAL,
 	},
-	.num_consumer_supplies	= ARRAY_SIZE(v2p8sx_consumer),
-	.consumer_supplies	= v2p8sx_consumer,
+	.num_consumer_supplies	= ARRAY_SIZE(xiaomi_mipad2_v2p8sx_consumer),
+	.consumer_supplies	= xiaomi_mipad2_v2p8sx_consumer,
 };
 /* 1v2psx regulator */
-static struct regulator_init_data v1p2sx_data = {
+static struct regulator_init_data xiaomi_mipad2_v1p2sx_data = {
 	.constraints = {
 		.min_uV			= 1200000,
 		.max_uV			= 1200000,
 		.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
 		.valid_modes_mask	= REGULATOR_MODE_NORMAL,
 	},
-	.num_consumer_supplies	= ARRAY_SIZE(v1p2sx_consumer),
-	.consumer_supplies	= v1p2sx_consumer,
+	.num_consumer_supplies	= ARRAY_SIZE(xiaomi_mipad2_v1p2sx_consumer),
+	.consumer_supplies	= xiaomi_mipad2_v1p2sx_consumer,
 };
 
 
-static struct regulator_init_data vprog4d_data = {
+static struct regulator_init_data xiaomi_mipad2_vprog4d_data = {
 	.constraints = {
 		.min_uV			= 2800000,
 		.max_uV			= 2800000,
 		.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
 		.valid_modes_mask	= REGULATOR_MODE_NORMAL,
 	},
-	.num_consumer_supplies	= ARRAY_SIZE(vprog4d_consumer),
-	.consumer_supplies	= vprog4d_consumer,
+	.num_consumer_supplies	= ARRAY_SIZE(xiaomi_mipad2_vprog4d_consumer),
+	.consumer_supplies	= xiaomi_mipad2_vprog4d_consumer,
 };
 
 /*************************************************************
@@ -725,64 +725,64 @@ static struct regulator_init_data vprog4d_data = {
 * WCOVE Camera related regulator
 *
 *************************************************************/
-static struct regulator_init_data wcove_v1p8sx_data;
-static struct regulator_init_data wcove_v2p8sx_data;
-static struct regulator_init_data wcove_v1p2sx_data;
-static struct regulator_init_data wcove_vprog4d_data;
+static struct regulator_init_data wcove_xiaomi_mipad2_v1p8sx_data;
+static struct regulator_init_data wcove_xiaomi_mipad2_v2p8sx_data;
+static struct regulator_init_data wcove_xiaomi_mipad2_v1p2sx_data;
+static struct regulator_init_data wcove_xiaomi_mipad2_vprog4d_data;
 
-static struct wcove_regulator_info wcove_v1p8sx_info = {
-	.init_data = &wcove_v1p8sx_data,
+static struct wcove_regulator_info wcove_xiaomi_mipad2_v1p8sx_info = {
+	.init_data = &wcove_xiaomi_mipad2_v1p8sx_data,
 };
 
-static struct wcove_regulator_info wcove_v2p8sx_info = {
-	.init_data = &wcove_v2p8sx_data,
+static struct wcove_regulator_info wcove_xiaomi_mipad2_v2p8sx_info = {
+	.init_data = &wcove_xiaomi_mipad2_v2p8sx_data,
 };
 
-static struct wcove_regulator_info wcove_v1p2sx_info = {
-	.init_data = &wcove_v1p2sx_data,
+static struct wcove_regulator_info wcove_xiaomi_mipad2_v1p2sx_info = {
+	.init_data = &wcove_xiaomi_mipad2_v1p2sx_data,
 };
 
-static struct wcove_regulator_info wcove_vprog4d_info = {
-	.init_data = &wcove_vprog4d_data,
+static struct wcove_regulator_info wcove_xiaomi_mipad2_vprog4d_info = {
+	.init_data = &wcove_xiaomi_mipad2_vprog4d_data,
 };
 
-static void wc_set_v1p8_pdata(void)
+static void xiaomi_mipad2_wc_set_v1p8_pdata(void)
 {
-	memcpy((void *)&wcove_v1p8sx_data, (void *)&v1p8sx_data,
+	memcpy((void *)&wcove_xiaomi_mipad2_v1p8sx_data, (void *)&xiaomi_mipad2_v1p8sx_data,
 			sizeof(struct regulator_init_data));
 
-	intel_soc_pmic_set_pdata("wcove_regulator", &wcove_v1p8sx_info,
+	intel_soc_pmic_set_pdata("wcove_regulator", &wcove_xiaomi_mipad2_v1p8sx_info,
 		sizeof(struct wcove_regulator_info), WCOVE_ID_V1P8SX + 1);
 
 }
 
-static void wc_set_v2p8_pdata(void)
+static void xiaomi_mipad2_wc_set_v2p8_pdata(void)
 {
-	memcpy((void *)&wcove_v2p8sx_data, (void *)&v2p8sx_data,
+	memcpy((void *)&wcove_xiaomi_mipad2_v2p8sx_data, (void *)&xiaomi_mipad2_v2p8sx_data,
 			sizeof(struct regulator_init_data));
 
 	/* register camera regulator for whiskey cove PMIC */
-	intel_soc_pmic_set_pdata("wcove_regulator", &wcove_v2p8sx_info,
+	intel_soc_pmic_set_pdata("wcove_regulator", &wcove_xiaomi_mipad2_v2p8sx_info,
 		sizeof(struct wcove_regulator_info), WCOVE_ID_V2P8SX + 1);
 }
 
-static void wc_set_v1p2_pdata(void)
+static void xiaomi_mipad2_wc_set_v1p2_pdata(void)
 {
-	memcpy((void *)&wcove_v1p2sx_data, (void *)&v1p2sx_data,
+	memcpy((void *)&wcove_xiaomi_mipad2_v1p2sx_data, (void *)&xiaomi_mipad2_v1p2sx_data,
 			sizeof(struct regulator_init_data));
 
 	/* register camera regulator for whiskey cove PMIC */
-	intel_soc_pmic_set_pdata("wcove_regulator", &wcove_v1p2sx_info,
+	intel_soc_pmic_set_pdata("wcove_regulator", &wcove_xiaomi_mipad2_v1p2sx_info,
 		sizeof(struct wcove_regulator_info), WCOVE_ID_V1P2SX + 1);
 }
 
-static void wc_set_vprog4d_pdata(void)
+static void xiaomi_mipad2_wc_set_vprog4d_pdata(void)
 {
-	memcpy((void *)&wcove_vprog4d_data, (void *)&vprog4d_data,
+	memcpy((void *)&wcove_xiaomi_mipad2_vprog4d_data, (void *)&xiaomi_mipad2_vprog4d_data,
 			sizeof(struct regulator_init_data));
 
 	/* register camera regulator for whiskey cove PMIC */
-	intel_soc_pmic_set_pdata("wcove_regulator", &wcove_vprog4d_info,
+	intel_soc_pmic_set_pdata("wcove_regulator", &wcove_xiaomi_mipad2_vprog4d_info,
 		sizeof(struct wcove_regulator_info), WCOVE_ID_VPROG4D + 1);
 }
 
@@ -910,10 +910,10 @@ static int whiskey_cove_init(void)
 	wcove_set_bcu_pdata();
 	wc_set_adc_pdata();
 	wc_set_gpio_pdata();
-	wc_set_v1p8_pdata();
-	wc_set_v2p8_pdata();
-	wc_set_v1p2_pdata();
-	wc_set_vprog4d_pdata();
+	xiaomi_mipad2_wc_set_v1p8_pdata();
+	xiaomi_mipad2_wc_set_v2p8_pdata();
+	xiaomi_mipad2_wc_set_v1p2_pdata();
+	xiaomi_mipad2_wc_set_vprog4d_pdata();
 	wc_set_thermal_pdata();
 	wcove_init_done = true;
 

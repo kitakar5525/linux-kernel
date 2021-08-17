ec2534d1f5939658629d22123029c9c003a85cfd
Tsuchiya Yuto <kitakar@gmail.com>
2021-08-18 01:14:15 +0900

    (DEBUG) atomisp: debug for ia_css_pipe_create() issue
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 6d9aeeed98a28de7c3ad73841b2d69fbc278d2b7)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 798bc695c0f31d3cd4a08fb136f4dc23c5465ecc)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 7b4a55e254922438ede3b7ce2d3b8d60a950137c)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 95cfd28903a545951b57e7c91c72c34b1c80ec8e)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/atomisp_driver/atomisp_subdev.c
+++ drivers/staging/media/atomisp/pci/atomisp_driver/atomisp_subdev.c
@@ -1109,7 +1109,16 @@ static int __atomisp_update_run_mode(struct atomisp_sub_device *asd)
 			CI_MODE_STILL_CAPTURE,
 			CI_MODE_CONTINUOUS,
 			CI_MODE_PREVIEW };
+	char *modes_str[] = { "CI_MODE_NONE",
+			      "CI_MODE_VIDEO",
+			      "CI_MODE_STILL_CAPTURE",
+			      "CI_MODE_CONTINUOUS",
+			      "CI_MODE_PREVIEW" };
 	s32 mode;
+
+	pr_alert("%s() called\n", __func__);
+	pr_alert("%s(): ctrl->val: %s\n", __func__, modes_str[ctrl->val]);
+	dump_stack();
 
 	if (ctrl->val != ATOMISP_RUN_MODE_VIDEO &&
 	    asd->continuous_mode->val)

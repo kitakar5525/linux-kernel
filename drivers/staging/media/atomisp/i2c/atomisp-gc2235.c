8370c2c741bb1a7a38b96a5d73772786984fe0f4
Tsuchiya Yuto <kitakar@gmail.com>
2021-08-17 16:34:36 +0900

    (aero) atomisp: i2c: fix include file name (-> atomisp_platform.h) for aero atomisp
    Fixes like:
    
            drivers/staging/media/atomisp/i2c/atomisp-mt9m114.c:33:10: fatal error: ../include/linux/atomisp_gmin_platform.h: No such file or directory
               33 | #include "../include/linux/atomisp_gmin_platform.h"
                  |          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/i2c/atomisp-gc2235.c
+++ drivers/staging/media/atomisp/i2c/atomisp-gc2235.c
@@ -29,7 +29,7 @@
 #include <linux/i2c.h>
 #include <linux/moduleparam.h>
 #include <media/v4l2-device.h>
-#include "../include/linux/atomisp_gmin_platform.h"
+#include "../include/linux/atomisp_platform.h"
 #include <linux/acpi.h>
 #include <linux/io.h>
 

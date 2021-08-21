3f08190a9871db0101f1b764f763c8bcf8b392f1
Tsuchiya Yuto <kitakar@gmail.com>
2021-08-21 20:45:56 +0900

    atomisp: pci: file/tpg: remove g_parm
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/atomisp_driver/atomisp_tpg.c
+++ drivers/staging/media/atomisp/pci/atomisp_driver/atomisp_tpg.c
@@ -31,12 +31,6 @@ static int tpg_s_stream(struct v4l2_subdev *sd, int enable)
 	return 0;
 }
 
-static int tpg_g_parm(struct v4l2_subdev *sd, struct v4l2_streamparm *param)
-{
-	/*to fake*/
-	return 0;
-}
-
 static int tpg_s_parm(struct v4l2_subdev *sd, struct v4l2_streamparm *param)
 {
 	/*to fake*/
@@ -151,7 +145,6 @@ static int tpg_enum_frame_ival(struct v4l2_subdev *sd,
 
 static const struct v4l2_subdev_video_ops tpg_video_ops = {
 	.s_stream = tpg_s_stream,
-	.g_parm = tpg_g_parm,
 	.s_parm = tpg_s_parm,
 	.enum_framesizes = tpg_enum_framesizes,
 	.enum_frameintervals = tpg_enum_frameintervals,

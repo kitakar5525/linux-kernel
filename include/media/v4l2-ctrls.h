469276423c836e600b68ee8e56970dd79dfc4b0a
Tsuchiya Yuto <kitakar@gmail.com>
2021-08-22 15:33:58 +0900

    Revert "(TEMP) (revert-for->=v4.8) [media] v4l2-subdev.h: resurrect the deprecated control subdev ops for now"
    This reverts commit 5d379df39fb207af5ccf80df1e4608d101792ea1.
    
    Time to convert.
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- include/media/v4l2-ctrls.h
+++ include/media/v4l2-ctrls.h
@@ -1415,16 +1415,6 @@ int v4l2_s_ext_ctrls(struct v4l2_fh *fh, struct v4l2_ctrl_handler *hdl,
 		     struct media_device *mdev,
 		     struct v4l2_ext_controls *c);
 
-/* Helpers for subdevices. If the associated ctrl_handler == NULL then they
-   will all return -EINVAL. */
-int v4l2_subdev_queryctrl(struct v4l2_subdev *sd, struct v4l2_queryctrl *qc);
-int v4l2_subdev_querymenu(struct v4l2_subdev *sd, struct v4l2_querymenu *qm);
-int v4l2_subdev_g_ext_ctrls(struct v4l2_subdev *sd, struct v4l2_ext_controls *cs);
-int v4l2_subdev_try_ext_ctrls(struct v4l2_subdev *sd, struct v4l2_ext_controls *cs);
-int v4l2_subdev_s_ext_ctrls(struct v4l2_subdev *sd, struct v4l2_ext_controls *cs);
-int v4l2_subdev_g_ctrl(struct v4l2_subdev *sd, struct v4l2_control *ctrl);
-int v4l2_subdev_s_ctrl(struct v4l2_subdev *sd, struct v4l2_control *ctrl);
-
 /**
  * v4l2_ctrl_subdev_subscribe_event - Helper function to implement
  *	as a &struct v4l2_subdev_core_ops subscribe_event function

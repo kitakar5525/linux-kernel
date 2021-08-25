714988d9650579c4aa40c65c94c593333ba8a06e
Tsuchiya Yuto <kitakar@gmail.com>
2021-08-25 20:54:56 +0900

    atomisp: pci: css: revert license changes back to irci_stable_candrpv_0415_20150521_0458
    Used the following script:
    
            dir="drivers/external_drivers/camera/drivers/media/pci/atomisp2/css"
            for f in $(git grep -w Copyright -- $dir | cut -f1 -d:); do
    
                sed -i $f \
                -e '1s@/\*\*@/\*@' \
                -e '2s@^@ \* @' \
                -e '3s@Copyright (c) 2010 - 2015, Intel Corporation.@ \* Copyright (c) 2015, Intel Corporation.@' \
                -e '4s@^@ \*@' \
                -e '5,7s@^@ \* @' \
                -e '8s@^@ \*@' \
                -e '9,12s@^@ \* @' \
                -e '13s@\*/@ *\/@'
    
                # add newline under license text if not exist
                l14="$(awk 'NR==14 {print}' $f)"
                if [ ! -z "$l14" ]; then
                    sed -e 14s@"${l14}"@"\n${l14}"@ $f -i
                fi
            done
    
    This patch basically does the following change:
    
    Before:
            /**
            Support for Intel Camera Imaging ISP subsystem.
            Copyright (c) 2010 - 2015, Intel Corporation.
    
            This program is free software; you can redistribute it and/or modify it
            under the terms and conditions of the GNU General Public License,
            version 2, as published by the Free Software Foundation.
    
            This program is distributed in the hope it will be useful, but WITHOUT
            ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
            FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
            more details.
            */
            FOO_BAR
    
    After:
            /*
             * Support for Intel Camera Imaging ISP subsystem.
             * Copyright (c) 2015, Intel Corporation.
             *
             * This program is free software; you can redistribute it and/or modify it
             * under the terms and conditions of the GNU General Public License,
             * version 2, as published by the Free Software Foundation.
             *
             * This program is distributed in the hope it will be useful, but WITHOUT
             * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
             * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
             * more details.
             */
            <NEWLINE>
            FOO_BAR
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/ia_css_system_ctrl.c
+++ drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/ia_css_system_ctrl.c
@@ -1,16 +1,17 @@
 /*
- * INTEL CONFIDENTIAL
+ *  * INTEL CONFIDENTIAL
  *
- * Copyright (C) 2015 Intel Corporation.
- * All Rights Reserved.
- *
- * The source code contained or described herein and all documents
- * related to the source code ("Material") are owned by Intel Corporation
- * or licensors. Title to the Material remains with Intel
- * Corporation or its licensors. The Material contains trade
- * secrets and proprietary and confidential information of Intel or its
- * licensors. The Material is protected by worldwide copyright
+ * * Copyright (C) 2015 Intel Corporation.
+ *  * All Rights Reserved.
+ *  *
+ *  * The source code contained or described herein and all documents
+ * * related to the source code ("Material") are owned by Intel Corporation
+ *  * or licensors. Title to the Material remains with Intel
+ *  * Corporation or its licensors. The Material contains trade
+ *  * secrets and proprietary and confidential information of Intel or its
+ *  * licensors. The Material is protected by worldwide copyright
  * and trade secret laws and treaty provisions. No part of the Material may
+ *
  * be used, copied, reproduced, modified, published, uploaded, posted,
  * transmitted, distributed, or disclosed in any way without Intel's prior
  * express written permission.

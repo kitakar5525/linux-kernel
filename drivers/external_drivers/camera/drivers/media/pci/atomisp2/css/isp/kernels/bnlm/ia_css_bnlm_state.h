841123fe83d6e32cff4d472de66f27924fa48181
Tsuchiya Yuto <kitakar@gmail.com>
2021-08-28 21:46:30 +0900

    Revert "atomisp2: css2400 and 2401 irci_ecr-master_20160121_0200"
    This reverts commit 0943ed689091b9d560abaed59c183de6338182ac.
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/isp/kernels/bnlm/ia_css_bnlm_state.h
+++ drivers/external_drivers/camera/drivers/media/pci/atomisp2/css/isp/kernels/bnlm/ia_css_bnlm_state.h
@@ -12,18 +12,20 @@ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 more details.
 */
 
-//
-// This file contains the version data for the CSS
-//
-// === Do not change - automatically generated ===
-//
-
-#ifndef __IA_CSS_VERSION_DATA_H
-#define __IA_CSS_VERSION_DATA_H
+#ifndef __IA_CSS_BNLM_STATE_H
+#define __IA_CSS_BNLM_STATE_H
 
 
-#define CSS_VERSION_STRING "REL:20160107_01.4_2023; API:2.1.21.0; GIT:irci___#428a21e7f937a777e612a8eaf5fec41478e09cd1; SDK:; USER:viedifw; "
+#include "type_support.h"
+#include "vmem.h" /* for VMEM_ARRAY*/
+#include "bnlm.isp.h"
+
+struct bnlm_vmem_state {
+	/* State buffers required for BNLM */
+	VMEM_ARRAY(buf[BNLM_STATE_BUF_HEIGHT], BNLM_STATE_BUF_WIDTH*ISP_NWAY);
+};
 
 
-#endif
+
+#endif /* __IA_CSS_BNLM_STATE_H */
 

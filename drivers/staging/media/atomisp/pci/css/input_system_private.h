b34e160a5ebfc3ba927c812f1afebeb75347685b
Tsuchiya Yuto <kitakar@gmail.com>
2021-09-01 23:14:38 +0900

    (fixup) atomisp: pci: css: properly test ISP version
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/css/input_system_private.h
+++ drivers/staging/media/atomisp/pci/css/input_system_private.h
@@ -4,4 +4,9 @@
  *    (c) 2020 Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
  */
 
+#ifdef ISP2401
+#  include "isp2401_input_system_private.h"
+#else
 #  include "isp2400_input_system_private.h"
+#endif
+

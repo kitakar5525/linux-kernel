778713da4fbfec18e5cfee29d86752ade922c4f5
Tsuchiya Yuto <kitakar@gmail.com>
2021-09-10 15:10:38 +0900

    atomisp: pci: css: revert ia_css_env.h to irci_stable_candrpv_0415_20150521_0458
            # f2a09a1e991f396e4ee2792415d4d9a2dd135916 ("atomisp: pci: delete files for irci_master_20150701_0213")
            git checkout --no-overlay f2a09a1e991f396e4ee2792415d4d9a2dd135916^ -- FILENAME
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/css/ia_css_env.h
+++ drivers/staging/media/atomisp/pci/css/ia_css_env.h
@@ -81,18 +81,6 @@ struct ia_css_css_mem_env {
 	ia_css_ptr (*mmap)(const void *ptr, const size_t size,
 			   uint16_t attribute, void *context);
 	/**< Map an pre-allocated memory region to an address. */
-
-	/* a set of matching functions with additional debug params */
-	ia_css_ptr(*alloc_ex)(size_t bytes, uint32_t attributes, const char *caller_func, int caller_line);
-	/**< same as alloc above, only with additional debug parameters */
-	void (*free_ex)(ia_css_ptr ptr, const char *caller_func, int caller_line);
-	/**< same as free above, only with additional debug parameters */
-	int (*load_ex)(ia_css_ptr ptr, void *data, size_t bytes, const char *caller_func, int caller_line);
-	/**< same as load above, only with additional debug parameters */
-	int (*store_ex)(ia_css_ptr ptr, const void *data, size_t bytes, const char *caller_func, int caller_line);
-	/**< same as store above, only with additional debug parameters */
-	int (*set_ex)(ia_css_ptr ptr, int c, size_t bytes, const char *caller_func, int caller_line);
-	/**< same as set above, only with additional debug parameters */
 };
 
 /** Environment with function pointers to access the CSS hardware. This includes

6911b03e010938ec7f0168993d7e9be42546cf0a
Tsuchiya Yuto <kitakar@gmail.com>
2021-09-13 21:35:27 +0900

    Revert "atomisp: pci: css: remove stop_copy_preview"
    This reverts commit 5cd09551f6215013e3dc4f6d39d40085a37d6ca1.
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/css/ia_css_stream.h
+++ drivers/staging/media/atomisp/pci/css/ia_css_stream.h
@@ -43,6 +43,7 @@ struct ia_css_stream {
 
 	bool                           cont_capt;
 	bool                           disable_cont_vf;
+	bool                           stop_copy_preview;
 	bool                           started;
 };
 

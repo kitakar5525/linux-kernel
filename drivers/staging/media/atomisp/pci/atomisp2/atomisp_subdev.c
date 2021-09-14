ca5fbecb2650d6c9c2421a0b724f7f559b016673
Tsuchiya Yuto <kitakar@gmail.com>
2021-09-14 13:02:17 +0900

    (port-to-v5.7) atomisp: atomisp_subdev.c: replace VFL_TYPE_GRABBER by VFL_TYPE_VIDEO
    Ref: e58eeb5a7335 ("media: atomisp: replace VFL_TYPE_GRABBER by VFL_TYPE_VIDEO")
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 4cb02783517fd73f9979f4bf28d5dc1362cefa18)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 69b67fec4ca283774a504aea2be75ec12937f6e1)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 22e3aaf6eb3384b3484fc22f42f9e2e8a6d8af21)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/atomisp2/atomisp_subdev.c
+++ drivers/staging/media/atomisp/pci/atomisp2/atomisp_subdev.c
@@ -1323,7 +1323,7 @@ int atomisp_subdev_register_entities(struct atomisp_sub_device *asd,
 	else
 		device_caps |= V4L2_CAP_VIDEO_OUTPUT;
 	asd->video_out_capture.vdev.device_caps = device_caps;
-	ret = video_register_device(video_dev, VFL_TYPE_GRABBER, -1);
+	ret = video_register_device(video_dev, VFL_TYPE_VIDEO, -1);
 	if (ret < 0)
 		goto error;
 
@@ -1335,7 +1335,7 @@ int atomisp_subdev_register_entities(struct atomisp_sub_device *asd,
 	else
 		device_caps |= V4L2_CAP_VIDEO_OUTPUT;
 	asd->video_out_vf.vdev.device_caps = device_caps;
-	ret = video_register_device(video_dev, VFL_TYPE_GRABBER, -1);
+	ret = video_register_device(video_dev, VFL_TYPE_VIDEO, -1);
 	if (ret < 0)
 		goto error;
 
@@ -1347,7 +1347,7 @@ int atomisp_subdev_register_entities(struct atomisp_sub_device *asd,
 	else
 		device_caps |= V4L2_CAP_VIDEO_OUTPUT;
 	asd->video_out_preview.vdev.device_caps = device_caps;
-	ret = video_register_device(video_dev, VFL_TYPE_GRABBER, -1);
+	ret = video_register_device(video_dev, VFL_TYPE_VIDEO, -1);
 	if (ret < 0)
 		goto error;
 
@@ -1359,7 +1359,7 @@ int atomisp_subdev_register_entities(struct atomisp_sub_device *asd,
 	else
 		device_caps |= V4L2_CAP_VIDEO_OUTPUT;
 	asd->video_out_video_capture.vdev.device_caps = device_caps;
-	ret = video_register_device(video_dev, VFL_TYPE_GRABBER, -1);
+	ret = video_register_device(video_dev, VFL_TYPE_VIDEO, -1);
 	if (ret < 0)
 		goto error;
 
@@ -1371,7 +1371,7 @@ int atomisp_subdev_register_entities(struct atomisp_sub_device *asd,
 	else
 		device_caps |= V4L2_CAP_VIDEO_OUTPUT;
 	asd->video_acc.vdev.device_caps = device_caps;
-	ret = video_register_device(video_dev, VFL_TYPE_GRABBER, -1);
+	ret = video_register_device(video_dev, VFL_TYPE_VIDEO, -1);
 	if (ret < 0)
 		goto error;
 
@@ -1390,7 +1390,7 @@ int atomisp_subdev_register_entities(struct atomisp_sub_device *asd,
 	else
 		device_caps |= V4L2_CAP_VIDEO_OUTPUT;
 	asd->video_in.vdev.device_caps = device_caps;
-	ret = video_register_device(video_dev, VFL_TYPE_GRABBER, -1);
+	ret = video_register_device(video_dev, VFL_TYPE_VIDEO, -1);
 	if (ret < 0)
 		goto error;
 

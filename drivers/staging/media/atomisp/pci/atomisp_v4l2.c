e149b1f8d5ce482be4ecab432c200dd06aeca4ab
Tsuchiya Yuto <kitakar@gmail.com>
2021-09-14 13:02:14 +0900

    (port-to-v5.4) atomisp: switch to use 'fallthrough' pseudo keyword for switch/case use
    The compiler started giving a error for the fallthrough without using
    the newly-added fallthrough keyword after commit 294f69e662d1
    ("compiler_attributes.h: Add 'fallthrough' pseudo keyword for
    switch/case use"). So, switch to use it here.
    
    Fixes like:
    
            drivers/staging/media/atomisp/pci/css2401a0_v21_build/../atomisp_driver/atomisp_v4l2.c: In function ‘atomisp_subdev_probe’:
            ./include/linux/dynamic_debug.h:122:52: error: this statement may fall through [-Werror=implicit-fallthrough=]
              122 | #define __dynamic_func_call(id, fmt, func, ...) do {    \
                  |                                                    ^
            ./include/linux/dynamic_debug.h:143:9: note: in expansion of macro ‘__dynamic_func_call’
              143 |         __dynamic_func_call(__UNIQUE_ID(ddebug), fmt, func, ##__VA_ARGS__)
                  |         ^~~~~~~~~~~~~~~~~~~
            ./include/linux/dynamic_debug.h:157:9: note: in expansion of macro ‘_dynamic_func_call’
              157 |         _dynamic_func_call(fmt,__dynamic_dev_dbg,               \
                  |         ^~~~~~~~~~~~~~~~~~
            ./include/linux/device.h:1751:9: note: in expansion of macro ‘dynamic_dev_dbg’
             1751 |         dynamic_dev_dbg(dev, dev_fmt(fmt), ##__VA_ARGS__)
                  |         ^~~~~~~~~~~~~~~
            drivers/staging/media/atomisp/pci/css2401a0_v21_build/../atomisp_driver/atomisp_v4l2.c:854:25: note: in expansion of macro ‘dev_dbg’
              854 |                         dev_dbg(isp->dev, "raw_index: %d\n", raw_index);
                  |                         ^~~~~~~
            drivers/staging/media/atomisp/pci/css2401a0_v21_build/../atomisp_driver/atomisp_v4l2.c:855:17: note: here
              855 |                 case SOC_CAMERA:
                  |                 ^~~~
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 39efe5d62021c80b0bead830729221bd37171e50)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 31e2e710c5189b81d4a8f1540ecce9a4351f7bc6)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 92ca20378ef03ed9afcc7fc92928f80089350cfc)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit ce1e699befb97c8a4ea6ff0f7b6199bb6439de68)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/atomisp_v4l2.c
+++ drivers/staging/media/atomisp/pci/atomisp_v4l2.c
@@ -1147,7 +1147,7 @@ static int atomisp_subdev_probe(struct atomisp_device *isp)
 		case RAW_CAMERA:
 			raw_index = isp->input_cnt;
 			dev_dbg(isp->dev, "raw_index: %d\n", raw_index);
-			/* pass-though */
+			fallthrough;
 		case SOC_CAMERA:
 			dev_dbg(isp->dev, "SOC_INDEX: %d\n", isp->input_cnt);
 			if (isp->input_cnt >= ATOM_ISP_MAX_INPUTS) {

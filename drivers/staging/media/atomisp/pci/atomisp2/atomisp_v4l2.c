33ecf43f74545e0c3385747060de054b3f66bf6b
Tsuchiya Yuto <kitakar@gmail.com>
2021-09-14 13:02:23 +0900

    atomisp: pci: move driver_version into "struct atomisp_device"
    Previously, it was in "struct media_device", but it was removed after
    commit 2bd8682375f3 ("media: media-device: remove driver_version") added
    on v4.14 series.
    
    So, re-add it into atomisp-specific struct "struct atomisp_device".
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 7ae3b919f8b6886090359f08b9b6667ab0425f24)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/atomisp2/atomisp_v4l2.c
+++ drivers/staging/media/atomisp/pci/atomisp2/atomisp_v4l2.c
@@ -1063,7 +1063,7 @@ atomisp_load_firmware(struct atomisp_device *isp)
 #if defined(ATOMISP_FWNAME)
 	fw_path = ATOMISP_FWNAME;
 #else
-	if (isp->media_dev.driver_version == ATOMISP_CSS_VERSION_21) {
+	if (isp->driver_version == ATOMISP_CSS_VERSION_21) {
 		if (isp->media_dev.hw_revision ==
 		    ((ATOMISP_HW_REVISION_ISP2401 << ATOMISP_HW_REVISION_SHIFT)
 		     | ATOMISP_HW_STEPPING_A0))
@@ -1084,7 +1084,7 @@ atomisp_load_firmware(struct atomisp_device *isp)
 	if (!fw_path) {
 		dev_err(isp->dev,
 			"Unsupported driver_version 0x%x, hw_revision 0x%x\n",
-			isp->media_dev.driver_version,
+			isp->driver_version,
 			isp->media_dev.hw_revision);
 		return NULL;
 	}
@@ -1222,7 +1222,7 @@ static int atomisp_pci_probe(struct pci_dev *dev,
 	/* This is not a true PCI device on SoC, so the delay is not needed. */
 	isp->pdev->d3hot_delay = 0;
 
-	isp->media_dev.driver_version = ATOMISP_CSS_VERSION_21;
+	isp->driver_version = ATOMISP_CSS_VERSION_21;
 	switch (id->device & ATOMISP_PCI_DEVICE_SOC_MASK) {
 	case ATOMISP_PCI_DEVICE_SOC_MRFLD:
 		isp->media_dev.hw_revision =

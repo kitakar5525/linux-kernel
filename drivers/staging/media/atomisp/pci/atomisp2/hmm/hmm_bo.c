7b7362ca3003748be7c86c38e3b5a76799ab52e3
Tsuchiya Yuto <kitakar@gmail.com>
2021-09-14 13:02:14 +0900

    (port-to-v5.4) atomisp: switch to use 'fallthrough' pseudo keyword for switch/case use
    The compiler started giving a error for the fallthrough without using
    the newly-added fallthrough keyword after commit 294f69e662d1
    ("compiler_attributes.h: Add 'fallthrough' pseudo keyword for
    switch/case use"). So, switch to use it here.
    
    Fixes like:
    
            drivers/staging/media/atomisp/pci/css2401a0_v21_build/../atomisp_driver/atomisp_v4l2.c: In function ‘atomisp_subdev_probe’:
            ./include/linux/dynamic_debug.h:122:52: error: this statement may fall through [-Werror=implicit-fallthrough=]
              122 | #define __dynamic_func_call(id, fmt, func, ...) do {    \
                  |                                                    ^
            ./include/linux/dynamic_debug.h:143:9: note: in expansion of macro ‘__dynamic_func_call’
              143 |         __dynamic_func_call(__UNIQUE_ID(ddebug), fmt, func, ##__VA_ARGS__)
                  |         ^~~~~~~~~~~~~~~~~~~
            ./include/linux/dynamic_debug.h:157:9: note: in expansion of macro ‘_dynamic_func_call’
              157 |         _dynamic_func_call(fmt,__dynamic_dev_dbg,               \
                  |         ^~~~~~~~~~~~~~~~~~
            ./include/linux/device.h:1751:9: note: in expansion of macro ‘dynamic_dev_dbg’
             1751 |         dynamic_dev_dbg(dev, dev_fmt(fmt), ##__VA_ARGS__)
                  |         ^~~~~~~~~~~~~~~
            drivers/staging/media/atomisp/pci/css2401a0_v21_build/../atomisp_driver/atomisp_v4l2.c:854:25: note: in expansion of macro ‘dev_dbg’
              854 |                         dev_dbg(isp->dev, "raw_index: %d\n", raw_index);
                  |                         ^~~~~~~
            drivers/staging/media/atomisp/pci/css2401a0_v21_build/../atomisp_driver/atomisp_v4l2.c:855:17: note: here
              855 |                 case SOC_CAMERA:
                  |                 ^~~~
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 984e7d956b81f4b56a5b258bd9265a22aaccb5fa)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit a5f06362d9e159d0e7e3c7dbc6449dee98efb9f0)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 9b2945c9e7d92bc5c8d9d60cdaf2f1ecee75039f)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit a1f11b06ea810f44f9426406af33b273fc7ca977)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/atomisp2/hmm/hmm_bo.c
+++ drivers/staging/media/atomisp/pci/atomisp2/hmm/hmm_bo.c
@@ -672,6 +672,8 @@ static void free_private_bo_pages(struct hmm_buffer_object *bo,
 				break;
 			}
 
+			fallthrough;
+
 			/*
 			 * if dynamic memory pool doesn't exist, need to free
 			 * pages to system directly.

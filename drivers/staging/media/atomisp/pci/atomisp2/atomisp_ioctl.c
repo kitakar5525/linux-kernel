2dca28a8cc8b7bf0a936a6743bce478059d8a881
Tsuchiya Yuto <kitakar@gmail.com>
2021-09-21 21:02:02 +0900

    atomisp: pci: remove atomisp_hw_is_isp2401 test part2
    Remove atomisp_hw_is_isp2401 test and adopt ISP2400 one.
    
    Used the following cocci script:
    
    	cat << EOS > cocci-script.cocci
    	// <smpl>
    
    	@@
    	statement S;
    	@@
    	- if (atomisp_hw_is_isp2401) {
    	- 	S
    	- }
    
    	// </smpl>
    	EOS
    
    	# apply changes for drivers/staging/media/atomisp
    	dirs="drivers/staging/media/atomisp"
    	for dir in $dirs; do
    		spatch --dir --include-headers --no-show-diff --in-place \
    		--iso-file /usr/lib/coccinelle/standard.iso \
    		--macro-file-builtins /usr/lib/coccinelle/standard.h \
    		--sp-file cocci-script.cocci $dir;
    	done
    
    	rm cocci-script.cocci
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/atomisp2/atomisp_ioctl.c
+++ drivers/staging/media/atomisp/pci/atomisp2/atomisp_ioctl.c
@@ -1730,8 +1730,6 @@ static int atomisp_streamon(struct file *file, void *fh,
 
 	/* Reset pending capture request count. */
 	asd->pending_capture_request = 0;
-	if (atomisp_hw_is_isp2401)
-		asd->re_trigger_capture = false;
 
 	if ((atomisp_subdev_streaming_count(asd) > sensor_start_stream) &&
 	    (!isp->inputs[asd->input_curr].camera_caps->multi_stream_ctrl)) {
@@ -1880,8 +1878,6 @@ start_sensor:
 		goto start_delay_wq;
 	} else if (asd->depth_mode->val && (atomisp_streaming_count(isp) <
 					    ATOMISP_DEPTH_SENSOR_STREAMON_COUNT)) {
-		if (atomisp_hw_is_isp2401)
-			__wdt_on_master_slave_sensor_pipe(pipe, wdt_duration, false);
 		goto start_delay_wq;
 	}
 

99bd45799370d745eb8fddc14ac62f3f6f25b7cb
Tsuchiya Yuto <kitakar@gmail.com>
2021-09-25 16:34:07 +0900

    (upst-fix) atomisp: pci: do not use `err` when checking port validity
    The `enum ia_css_err err` is set to `IA_CSS_ERR_INTERNAL_ERROR` by
    default, but the `err` is not used before these port validity check
    for !IS_ISP2401 case. So, this always results in early return here,
    like the following log:
    
    	kern  :err   : [303247.394596] atomisp-isp2 0000:00:03.0: can't change power state from D3cold to D0 (config space inaccessible)
    	kern  :warn  : [303247.406367] isys dma store at addr(0xcd408) val(0)
    	kern  :warn  : [303247.567741] atomisp-isp2 0000:00:03.0: assuming run_mode is PREVIEW
    	kern  :warn  : [303247.799250] allocate_mipi_frames() 443: error: allocate_mipi_frames(00000000609f9ce9) exit: port is not correct (port=0).
    	kern  :err   : [303247.799273] atomisp-isp2 0000:00:03.0: stream[0] start error.
    	kern  :warn  : [303247.799327] free_mipi_frames() 585: error: free_mipi_frames(00000000609f9ce9, 0) exit: pipe port is not correct.
    	kern  :warn  : [303247.860478] atomisp-isp2 0000:00:03.0: atomisp_release: ISP still streaming while closing!
    	kern  :err   : [303247.862543] atomisp-isp2 0000:00:03.0: atomisp_reset
    	kern  :warn  : [303247.875268] isys dma store at addr(0xcd408) val(0)
    
    Fix it by removing `err` value check.
    
    Fixes: 3c0538fbad9f ("media: atomisp: get rid of most checks for ISP2401 version")
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit a05e084a4fa29a91108b18c72ef1428d93aacef0)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 795099ae23ad0109734bb85d4a31ecfc60368d64)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>
    (cherry picked from commit 10dd7918acc65c8d7833ccc3153154cdfce9112b)
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- drivers/staging/media/atomisp/pci/sh_css_mipi.c
+++ drivers/staging/media/atomisp/pci/sh_css_mipi.c
@@ -399,7 +399,7 @@ allocate_mipi_frames(struct ia_css_pipe *pipe,
 
 	assert(port < N_CSI_PORTS);
 
-	if (port >= N_CSI_PORTS || err) {
+	if (port >= N_CSI_PORTS) {
 		ia_css_debug_dtrace(IA_CSS_DEBUG_TRACE_PRIVATE,
 				    "allocate_mipi_frames(%p) exit: error: port is not correct (port=%d).\n",
 				    pipe, port);
@@ -545,7 +545,7 @@ free_mipi_frames(struct ia_css_pipe *pipe) {
 
 		assert(port < N_CSI_PORTS);
 
-		if (port >= N_CSI_PORTS || err) {
+		if (port >= N_CSI_PORTS) {
 			ia_css_debug_dtrace(IA_CSS_DEBUG_TRACE_PRIVATE,
 					    "free_mipi_frames(%p, %d) exit: error: pipe port is not correct.\n",
 					    pipe, port);
@@ -654,7 +654,7 @@ send_mipi_frames(struct ia_css_pipe *pipe) {
 
 	assert(port < N_CSI_PORTS);
 
-	if (port >= N_CSI_PORTS || err) {
+	if (port >= N_CSI_PORTS) {
 		IA_CSS_ERROR("send_mipi_frames(%p) exit: invalid port specified (port=%d).\n",
 			     pipe, port);
 		return err;
